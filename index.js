546604463181ae38ceae974559eb0ec3b1e2b9a9
HomeAlone <luke.gallagher.cst@gmail.com>
2020-10-14 14:05:17 +1100

    added inquirer, with questions and save answer to file

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
+var inquirer = require("inquirer");
+var fs = require('fs');
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "Enter the Project title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter the Project description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter installation instructions:",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Enter project usage information:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter contribution guidelines:",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "Enter test instructions:",
+        name: "testing"
+    },
+    {
+        type: "input",
+        message: "Enter installation instructions:",
+        name: "install"
+    },
+    {
+        type: "list",
+        message: "Select a license for the project:",
+        name: "contact",
+        choices: [
+            "None",
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Caluse 'Simplified' License",
+            "BSD 3-Caluse 'New' or 'Revised' License",
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU General Public License v2.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unilicense"
+        ]
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "gituser"
+    },
+    {
+        type: "input",
+        message: "Enter your email address:",
+        name: "email"
+    },
 
 ];
 
@@ -9,7 +78,22 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    // prompt for the questions
+    inquirer.prompt(questions).then(function (data) {
 
+        // on return, for now save to file to test data
+        var filename = data.title.toLowerCase().split(' ').join('') + ".json";
+
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("Success!");
+
+        });
+    });
 }
 
 // function call to initialize program
