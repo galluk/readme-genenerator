e727143b632cf642d013ef8216f89461f1901bf2
HomeAlone <luke.gallagher.cst@gmail.com>
2020-10-14 15:38:27 +1100

    first iteration of readme structure, fixed exports code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${data.description}
 
+  ## Description 
+   
+  ## Table of Contents
+  [*Installation](#-installation)
+  [*Usage](#-usage)
+  [*License](#-license)
+  [*Contributing](#-contributing)
+  [*Tests](#-tests)
+  [*Questions](#-questions)
+
+  ### Installation
+  ${data.install}
+
+  ### Usage
+  ${data.usage}
+
+  ### License
+  ${data.license}
+
+  ### Contributing
+  ${data.contribute}
+  
+  ### Tests
+  ${data.testing}
+
+  ### Questions
+  To find out more see my github at https://github.com/${data.github}.
+
+  For questions or to find out more about this project please email ${data.email}.
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown: generateMarkdown
+};
